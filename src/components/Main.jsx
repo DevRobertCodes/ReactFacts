32f6adadfe1260a666d74b4e709b9f4c2f42e775
Robert <devrobertcodes@gmail.com>
2025-07-13 02:05:39 +0000

    Initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return(
+        <main>
+          <h1>Fun facts about React</h1>
+          <ul className="facts-list">
+            <li className="list-items">Was first released in 2013</li>
+            <li className="list-items">Was originally created by Jordan Walke</li>
+            <li className="list-items">Has well over 100k stars on GitHub</li>
+            <li className="list-items">Is maintained by Meta</li>
+            <li className="list-items">Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return(
+          <header>
+            <nav>
+                <img src="/src/assets/react.svg" alt="React-Logo" />
+                <span className="null">ReactFacts</span>
+            </nav>
+          </header>
+    )
+}
